12979185fbe0eeb3410e0c6e538e7658cebe5fa3
IN_MOURITECH\rushikeshm <rushikeshm.in@mouritech.com>
2022-03-27 19:41:05 +0530

    worked on lists by using map and functions

---
+++ Assignment-03 Working with Lists/src/components/Expenses/Expenses.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+
+function Expenses(props) {
+  const { items } = props;
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
